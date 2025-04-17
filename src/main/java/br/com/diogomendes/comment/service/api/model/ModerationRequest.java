bf307460f735717960bd54ed0543b455b83c2cf5
didamendes <didamendes@hotmail.com>
2025-04-17 09:27:51 -0300

    Projeto finalizado

---
+++ src/main/java/br/com/diogomendes/comment/service/api/model/ModerationRequest.java
@@ -0,0 +1,17 @@
+package br.com.diogomendes.comment.service.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ModerationRequest {
+    private UUID commentId;
+    private String text;
+}
